7ab7848ed95b7acc8bedb9143f28baf7e0689dda
rocco337 <roko.bobic@gmail.com>
2015-01-25 23:12:31 +0100

    initial checkin

---
+++ Tripper.Services.Core/DependecyResolver.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tripper.Services.Core
+{
+    public class DependecyResolver
+    {
+        public static ContainerBuilder ServicesModule()
+        {
+            var builder = new ContainerBuilder();
+          
+            builder.Register(c => new TripService()).As<ITripService>().InstancePerRequest();
+
+            return builder;
+        }
+    }
+}
